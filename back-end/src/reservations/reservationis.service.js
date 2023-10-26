eabc8ec705d41727fcd7e01ecfd716ebfc12beb3
Jairus Ezra DuBose <dubosejairus@gmail.com>
2023-10-26 00:31:52 -0400

    Updated reservation.service and controller.js

--- back-end/src/reservations/reservationis.service.js
+++ back-end/src/reservations/reservationis.service.js
@@ -17,8 +17,33 @@ function create(reservation){
     return knex("reservations").insert(reservation).returning("*").then((createdReservation) => createdReservation[0]);
 }
 
+function createTable(newTable){
+  return knex("table").insert(newTable).select("*").then((createdTable) => createdTable[0]);
+}
+
+function read(reservation_id){
+  return knex("reservation").select("*").where({reservation_id}).then((selectedReservation) => selectedReservation[0]);
+}
+
+function listReservationForDate(date){
+  return knex("reservation").select("*").where({reservation_date: date}).whereNot({status: "finished"}).orderBy("reservation_time");
+}
+
+function listTables(){
+  return knex("tables").select("*").orderBy("table_name");
+}
+
+function updateReservationStatus(reservation_id, newStatus){
+  return knex("reservations").where({reservation_id}).update({status: newStatus}, "*");
+}
+
 module.exports = {
     list,
+    listTables,
+    listReservationForDate,
     search,
     create,
+    createTable,
+    read,
+    updateReservationStatus,
 };
--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -2,8 +2,18 @@
  * List handler for reservation resources
  */
 const reservationsService = require("./reservationis.service");
-
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const hasProperties = require("../errors/hasProperties");
+
+const hasRequiredProperties = hasProperties(
+  "first_name",
+  "last_name",
+  "mobile_number",
+  "reservation_date",
+  "reservation_time",
+  "reservation_time",
+  "people"
+);
 
 async function create(req, res){
   const data = await reservationsService.create(req.body.data);
@@ -12,12 +22,88 @@ async function create(req, res){
 
 async function list(req, res) {
   
-  const date = req.query.date;
-  const mobile_number = req.query.mobile_number;
-  const data = await (date ? reservationsService.list(date) : reservationsService.search(mobile_number))
-  
-  res.json({data});
+  const {date, currentDate} = req.query;
+  if(date){
+    const reservations = await reservationsService.listReservationsForDate(date);
+    res.json({data: reservations});
+  }else if (currentDate){
+    const reservations = await reservationsService.listReservationForDate(currentDate);
+    res.json({data: reservations});
+  }else{
+    const reservations = await reservationsService.list();
+    res.json({data: reservations});
+  }
+
 }
+
+async function listAllReservations(req, res,next){
+  const reservations = await reservationsService.list();
+  res.json({data: reservations});
+}
+
+async function listTable(req, res, next){
+  const data = res.locals.reservation;
+  res.status(200).json({data});
+}
+
+async function create(req, res){
+  const data = await reservationsService.create(req.body.data);
+  res.status(201).json({data});
+}
+
+
+async function createTable(req, res,next){
+  const data = await reservationsService.create(req.body.data);
+  res.status(201).json({data});
+}
+
+function read(req,res,next){
+  const data = res.locals.reservation;
+  res.status(200).json({data})
+}
+
+async function updatedReservationStatus(req, res,next){
+  const {status} = req.body.data;
+  const reservation = res.locals.reservation;
+  const data = await reservationsService.updatedReservationStatus(
+    reservation.reservation_id,
+    status
+  );
+}
+
+async function reservationExists(req, res, next){
+  const {reservation_id} = req.params;
+  const reservation = await reservationsService.read(reservation_id);
+  if(reservation){
+    res.locals.reservation = reservation;
+    return next(); 
+  }
+}
+
+function notFinishedForUpdate(req, res, next){
+  const reservation = res.locals.reservation;
+  if (reservation.status === "finished") {
+    next({
+      status: 400,
+      message: "reservation cannot already be finished.",
+    });
+  } else {
+    return next();
+  }
+}
+
+function upddatedValidationStatus(req,res,next){
+  const status = req.body.data.status;
+  if (status !== "unknown") {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "status cannot be unknown.",
+  });
+}
+
+
 
 module.exports = {
   create: asyncErrorBoundary(create),
