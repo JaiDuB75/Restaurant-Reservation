7fe4cff87d4dec186fd650ce77f103084b2c59a5
Jairus Ezra DuBose <dubosejairus@gmail.com>
2023-11-02 00:41:36 -0400

    Added Logic for the Reservations and Tables

---
+++ front-end/src/Reservations/NewReservation.js
@@ -0,0 +1,58 @@
+import React, {useState} from "react";
+import {useHistory} from "react-router-dom";
+import {createReservation} from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert"; 
+import ReservationLayout from "./ReservationLayout";
+
+function NewReservation(){
+    const history = useHistory();
+
+  const [newReservation, setNewReservation] = useState({
+    first_name: "",
+    last_name: "",
+    mobile_number: "",
+    reservation_date: "",
+    reservation_time: "",
+    people: "",
+  });
+
+  const [reservationError, setReservationError] = useState(false);
+
+  const handleChange = (event) => {
+    let { name, value } = event.target;
+    if (name === "people") {
+      value = Number(value);
+    }
+    setNewReservation((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const abortController = new AbortController();
+    try {
+      await createReservation(newReservation, abortController.signal);
+      history.push(`/dashboard?date=${newReservation.reservation_date}`);
+    } catch (error) {
+      setReservationError(error);
+    }
+    return () => abortController.abort();
+  };
+
+  return (
+    <>
+      <h2>New Reservation</h2>
+      <ErrorAlert error={reservationError} />
+      <ReservationForm
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+        history={history}
+        reservation={newReservation}
+      />
+    </>
+  );
+}
+
+export default NewReservation;
---
+++ front-end/src/Tables/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TableLayout from "./TableLayout";
+
+function Tables() {
+  return (
+    <>
+      <h2>New Table</h2>
+      <TableLayout />
+    </>
+  );
+}
+
+export default Tables;
